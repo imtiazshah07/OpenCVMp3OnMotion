3f466bf9c899d241bfee2fe7e63fae1072ea9ea9
Imtiaz Ali Shah Syed <imtiazshah07@gmail.com>
2017-10-31 20:15:14 +0100

    Update CaptureMotions.py

--- src/com/smartypro/main/CaptureMotions.py
+++ src/com/smartypro/main/CaptureMotions.py
@@ -62,7 +62,7 @@ class CaptureMotions (threading.Thread):
             thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
             
             thresh = cv2.dilate(thresh, None, iterations=2)
-            (_,cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
             
             for c in cnts:
                 if cv2.contourArea(c) > self._contourAreaMin and cv2.contourArea(c) < self._contourAreaMax: 
